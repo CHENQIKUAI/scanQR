9f81be884670e25837e9ce66a070e6454b58ded8
陈其快 <chenqk@33.cn>
2021-04-29 19:13:07 +0800

    123

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts("precache-manifest.4faaddb85947e18f5779fbf86acc88cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.routing.registerRoute(
-    /.*/,
+    /.*[^\/]$/,
     new workbox.strategies.StaleWhileRevalidate()
 );
