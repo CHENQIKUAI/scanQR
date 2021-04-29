d951f40e114d997a2121e9a9b3a3d29ad952c832
陈其快 <chenqk@33.cn>
2021-04-29 18:48:05 +0800

    123

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,28 @@
 importScripts("precache-manifest.4faaddb85947e18f5779fbf86acc88cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.routing.registerRoute(
-    new RegExp('.*'),
-    new workbox.strategies.CacheFirst()
+    /\.(?:css|js|html)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        "cacheName": 'assets',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60,
+            })
+        ]
+    })
+);
+
+
+workbox.routing.registerRoute(
+    /\.(?:gif|jpg|png)$/,
+    new workbox.strategies.CacheFirst({
+        "cacheName": 'images',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60,
+            })
+        ]
+    })
 );
