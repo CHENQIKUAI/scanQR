7f093b4af56fb3ad0e8ac7d269453b4d585f30b7
陈其快 <chenqk@33.cn>
2021-04-29 18:05:20 +0800

    '123

--- precache-manifest.4faaddb85947e18f5779fbf86acc88cc.js
+++ precache-manifest.4faaddb85947e18f5779fbf86acc88cc.js
@@ -1,217 +1,217 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "087dcd61dcabe0679b31",
-    "url": "/css/app.1b38c9a4.css"
+    "revision": "493388eae4b93815bc86",
+    "url": "css/app.1b38c9a4.css"
   },
   {
-    "revision": "09f7385ea9ec3ef86a4f",
-    "url": "/css/chunk-6bb53e0e.5027d4f7.css"
+    "revision": "029cc7148401ec44a6b8",
+    "url": "css/chunk-6bb53e0e.5027d4f7.css"
   },
   {
     "revision": "b71eb83ed943d3afc61c",
-    "url": "/css/chunk-vendors.690d4069.css"
+    "url": "css/chunk-vendors.690d4069.css"
   },
   {
     "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
-    "url": "/favicon.ico"
+    "url": "favicon.ico"
   },
   {
     "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
-    "url": "/fonts/Material-Design-Iconic-Font.a4d31128.woff2"
+    "url": "fonts/Material-Design-Iconic-Font.a4d31128.woff2"
   },
   {
     "revision": "b351bd62abcd96e924d9f44a3da169a7",
-    "url": "/fonts/Material-Design-Iconic-Font.b351bd62.ttf"
+    "url": "fonts/Material-Design-Iconic-Font.b351bd62.ttf"
   },
   {
     "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
-    "url": "/fonts/Material-Design-Iconic-Font.d2a55d33.woff"
+    "url": "fonts/Material-Design-Iconic-Font.d2a55d33.woff"
   },
   {
     "revision": "2ef8ba3410dcc71578a880e7064acd7a",
-    "url": "/fonts/fa-brands-400.2ef8ba34.woff"
+    "url": "fonts/fa-brands-400.2ef8ba34.woff"
   },
   {
     "revision": "5e2f92123d241cabecf0b289b9b08d4a",
-    "url": "/fonts/fa-brands-400.5e2f9212.woff2"
+    "url": "fonts/fa-brands-400.5e2f9212.woff2"
   },
   {
     "revision": "98b6db59be947f563350d2284fc9ea36",
-    "url": "/fonts/fa-brands-400.98b6db59.ttf"
+    "url": "fonts/fa-brands-400.98b6db59.ttf"
   },
   {
     "revision": "a7b95dbdd87e0c809570affaf366a434",
-    "url": "/fonts/fa-brands-400.a7b95dbd.eot"
+    "url": "fonts/fa-brands-400.a7b95dbd.eot"
   },
   {
     "revision": "427d721b86fc9c68b2e85ad42b69238c",
-    "url": "/fonts/fa-regular-400.427d721b.woff"
+    "url": "fonts/fa-regular-400.427d721b.woff"
   },
   {
     "revision": "65b9977aa23185e8964b36eddbce7a20",
-    "url": "/fonts/fa-regular-400.65b9977a.ttf"
+    "url": "fonts/fa-regular-400.65b9977a.ttf"
   },
   {
     "revision": "dcce4b7fbd5e895561e18af4668265af",
-    "url": "/fonts/fa-regular-400.dcce4b7f.eot"
+    "url": "fonts/fa-regular-400.dcce4b7f.eot"
   },
   {
     "revision": "e6257a726a0cf6ec8c6fec22821c055f",
-    "url": "/fonts/fa-regular-400.e6257a72.woff2"
+    "url": "fonts/fa-regular-400.e6257a72.woff2"
   },
   {
     "revision": "418dad87601f9c8abd0e5798c0dc1feb",
-    "url": "/fonts/fa-solid-900.418dad87.woff2"
+    "url": "fonts/fa-solid-900.418dad87.woff2"
   },
   {
     "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
-    "url": "/fonts/fa-solid-900.46e7cec6.eot"
+    "url": "fonts/fa-solid-900.46e7cec6.eot"
   },
   {
     "revision": "a7140145ebaaf5fb14e40430af5d25c4",
-    "url": "/fonts/fa-solid-900.a7140145.woff"
+    "url": "fonts/fa-solid-900.a7140145.woff"
   },
   {
     "revision": "ff8d9f8adb0d09f11d4816a152672f53",
-    "url": "/fonts/fa-solid-900.ff8d9f8a.ttf"
+    "url": "fonts/fa-solid-900.ff8d9f8a.ttf"
   },
   {
     "revision": "7f9fdd5e7c0656fea97141765f3e0b50",
-    "url": "/fonts/ionicons.7f9fdd5e.eot"
+    "url": "fonts/ionicons.7f9fdd5e.eot"
   },
   {
     "revision": "96f1c901c087fb64019f7665f7f8aca6",
-    "url": "/fonts/ionicons.96f1c901.woff2"
+    "url": "fonts/ionicons.96f1c901.woff2"
   },
   {
     "revision": "c37ad37a3a23417b739ac3b297416201",
-    "url": "/fonts/ionicons.c37ad37a.woff"
+    "url": "fonts/ionicons.c37ad37a.woff"
   },
   {
     "revision": "cfdc15225683b7529d6ba1e9d8a9be59",
-    "url": "/fonts/ionicons.cfdc1522.ttf"
+    "url": "fonts/ionicons.cfdc1522.ttf"
   },
   {
     "revision": "5bf145531213545e03ff41cd27df7d2b",
-    "url": "/img/fa-brands-400.5bf14553.svg"
+    "url": "img/fa-brands-400.5bf14553.svg"
   },
   {
     "revision": "5eb754ab7dbd2fee562360528db4c3c0",
-    "url": "/img/fa-regular-400.5eb754ab.svg"
+    "url": "img/fa-regular-400.5eb754ab.svg"
   },
   {
     "revision": "49279363201ed19a840796e05a74a91b",
-    "url": "/img/fa-solid-900.49279363.svg"
+    "url": "img/fa-solid-900.49279363.svg"
   },
   {
     "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
-    "url": "/img/icons/android-chrome-192x192.png"
+    "url": "img/icons/android-chrome-192x192.png"
   },
   {
     "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
-    "url": "/img/icons/android-chrome-512x512.png"
+    "url": "img/icons/android-chrome-512x512.png"
   },
   {
     "revision": "845a39478d0e2d4d5d32a092de2de250",
-    "url": "/img/icons/android-chrome-maskable-192x192.png"
+    "url": "img/icons/android-chrome-maskable-192x192.png"
   },
   {
     "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
-    "url": "/img/icons/android-chrome-maskable-512x512.png"
+    "url": "img/icons/android-chrome-maskable-512x512.png"
   },
   {
     "revision": "936d6e411cabd71f0e627011c3f18fe2",
-    "url": "/img/icons/apple-touch-icon-120x120.png"
+    "url": "img/icons/apple-touch-icon-120x120.png"
   },
   {
     "revision": "1a034e64d80905128113e5272a5ab95e",
-    "url": "/img/icons/apple-touch-icon-152x152.png"
+    "url": "img/icons/apple-touch-icon-152x152.png"
   },
   {
     "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
-    "url": "/img/icons/apple-touch-icon-180x180.png"
+    "url": "img/icons/apple-touch-icon-180x180.png"
   },
   {
     "revision": "9a2b5c0f19de617685b7b5b42464e7db",
-    "url": "/img/icons/apple-touch-icon-60x60.png"
+    "url": "img/icons/apple-touch-icon-60x60.png"
   },
   {
     "revision": "af28d69d59284dd202aa55e57227b11b",
-    "url": "/img/icons/apple-touch-icon-76x76.png"
+    "url": "img/icons/apple-touch-icon-76x76.png"
   },
   {
     "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
-    "url": "/img/icons/apple-touch-icon.png"
+    "url": "img/icons/apple-touch-icon.png"
   },
   {
     "revision": "4bb1a55479d61843b89a2fdafa7849b3",
-    "url": "/img/icons/favicon-16x16.png"
+    "url": "img/icons/favicon-16x16.png"
   },
   {
     "revision": "98b614336d9a12cb3f7bedb001da6fca",
-    "url": "/img/icons/favicon-32x32.png"
+    "url": "img/icons/favicon-32x32.png"
   },
   {
     "revision": "b89032a4a5a1879f30ba05a13947f26f",
-    "url": "/img/icons/msapplication-icon-144x144.png"
+    "url": "img/icons/msapplication-icon-144x144.png"
   },
   {
     "revision": "058a3335d15a3eb84e7ae3707ba09620",
-    "url": "/img/icons/mstile-150x150.png"
+    "url": "img/icons/mstile-150x150.png"
   },
   {
     "revision": "6de16254d1b4193353e7015f9177535d",
-    "url": "/img/icons/safari-pinned-tab.svg"
+    "url": "img/icons/safari-pinned-tab.svg"
   },
   {
     "revision": "665921072642ed354618b32af7425a22",
-    "url": "/img/ionicons.66592107.svg"
+    "url": "img/ionicons.66592107.svg"
   },
   {
-    "revision": "c6b3cf5b6d007e2496f2926678968428",
-    "url": "/index.html"
+    "revision": "53a319f0fbaf4dafbe6f68d590fdfa40",
+    "url": "index.html"
   },
   {
-    "revision": "087dcd61dcabe0679b31",
-    "url": "/js/app.f1a89abc.js"
+    "revision": "493388eae4b93815bc86",
+    "url": "js/app.e7b46468.js"
   },
   {
     "revision": "70b2585b34c79cc47013",
-    "url": "/js/chunk-2d216635.5c7f31ab.js"
+    "url": "js/chunk-2d216635.5c7f31ab.js"
   },
   {
     "revision": "9181bdd82f25b32ac2c6",
-    "url": "/js/chunk-2d217188.84e01e33.js"
+    "url": "js/chunk-2d217188.84e01e33.js"
   },
   {
     "revision": "72b3365b130c6f234aaa",
-    "url": "/js/chunk-2d21a3d2.c93e24c0.js"
+    "url": "js/chunk-2d21a3d2.c93e24c0.js"
   },
   {
     "revision": "8ee06eeeb654b8029714",
-    "url": "/js/chunk-2d22250d.a29e5ca3.js"
+    "url": "js/chunk-2d22250d.a29e5ca3.js"
   },
   {
-    "revision": "09f7385ea9ec3ef86a4f",
-    "url": "/js/chunk-6bb53e0e.db0a9275.js"
+    "revision": "029cc7148401ec44a6b8",
+    "url": "js/chunk-6bb53e0e.ef7a7b7b.js"
   },
   {
     "revision": "b71eb83ed943d3afc61c",
-    "url": "/js/chunk-vendors.46c6a16c.js"
+    "url": "js/chunk-vendors.46c6a16c.js"
   },
   {
     "revision": "53a67ec8f59ff9c9c4a3528a1ff2249e",
-    "url": "/manifest.json"
+    "url": "manifest.json"
   },
   {
-    "url": "/precache-manifest.8b2a26e7a2cd3b3eb5f6fd1c34444ed4.js"
+    "url": "precache-manifest.8f526cdb833ef5a0ea654ae26fb4f899.js"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   },
   {
-    "revision": "b12a87c4aa12ae2d3b8761d9de756e30",
-    "url": "/service-worker.js"
+    "revision": "6433b9b7c73a50e16d4cfb5f286b9339",
+    "url": "service-worker.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3a2f916c2ac856207e7526b848b9ef5a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.4faaddb85947e18f5779fbf86acc88cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.routing.registerRoute(
     new RegExp('.*'),
